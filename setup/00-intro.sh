98b39fe5ca635dcaafd99005759b444c7010ae93
Viktor Farcic <viktor@farcic.com>
2023-12-23 19:47:12 +0100

    Intro

---
+++ setup/00-intro.sh
@@ -0,0 +1,159 @@
+#!/bin/sh
+set -e
+
+gum style \
+	--foreground 212 --border-foreground 212 --border double \
+	--margin "1 2" --padding "2 4" \
+	'Setup for the Security chapter.
+  
+This script assumes that you jumped straight into this chapter.
+If that is not the case (if you are continuing from the previous
+chapter), please answer with "No" when asked whether you are
+ready to start.'
+
+gum confirm '
+Are you ready to start?
+Select "Yes" only if you did NOT follow the story from the start (if you jumped straight into this chapter).
+Feel free to say "No" and inspect the script if you prefer setting up resources manually.
+' || exit 0
+
+echo "
+## You will need following tools installed:
+|Name            |Required             |More info                                          |
+|----------------|---------------------|---------------------------------------------------|
+|Linux Shell     |Yes                  |Use WSL if you are running Windows                 |
+|Docker          |Yes                  |'https://docs.docker.com/engine/install'           |
+|kind CLI        |Yes                  |'https://kind.sigs.k8s.io/docs/user/quick-start/#installation'|
+|kubectl CLI     |Yes                  |'https://kubernetes.io/docs/tasks/tools/#kubectl'  |
+|crossplane CLI  |Yes                  |'https://docs.crossplane.io/latest/cli'            |
+|yq CLI          |Yes                  |'https://github.com/mikefarah/yq#install'          |
+|Google Cloud account with admin permissions|If using Google Cloud|'https://cloud.google.com'|
+|Google Cloud CLI|If using Google Cloud|'https://cloud.google.com/sdk/docs/install'        |
+|AWS account with admin permissions|If using AWS|'https://aws.amazon.com'                  |
+|AWS CLI         |If using AWS         |'https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html'|
+|eksctl CLI      |If using AWS         |'https://eksctl.io/installation/'                  |
+|Azure account with admin permissions|If using Azure|'https://azure.microsoft.com'         |
+|az CLI          |If using Azure       |'https://learn.microsoft.com/cli/azure/install-azure-cli'|
+" | gum format
+
+gum confirm "
+Do you have those tools installed?
+" || exit 0
+
+#########################
+# Control Plane Cluster #
+#########################
+
+kind create cluster
+
+##############
+# Crossplane #
+##############
+
+helm upgrade --install crossplane crossplane \
+    --repo https://charts.crossplane.io/stable \
+    --namespace crossplane-system --create-namespace --wait
+
+kubectl apply \
+    --filename providers/provider-kubernetes-incluster.yaml
+
+kubectl apply --filename providers/provider-helm-incluster.yaml
+
+kubectl apply --filename providers/dot-kubernetes.yaml
+
+kubectl apply --filename providers/dot-sql.yaml
+
+kubectl apply --filename providers/dot-app.yaml
+
+gum spin --spinner dot \
+    --title "Waiting for Crossplane providers..." -- sleep 60
+
+kubectl wait --for=condition=healthy provider.pkg.crossplane.io \
+    --all --timeout=600s
+
+echo "
+Which Hyperscaler do you want to use?"
+
+HYPERSCALER=$(gum choose "google" "aws" "azure")
+
+echo "export HYPERSCALER=$HYPERSCALER" >> .env
+
+export KUBECONFIG=$PWD/kubeconfig.yaml
+
+echo "export KUBECONFIG=$KUBECONFIG" >> .env
+
+if [[ "$HYPERSCALER" == "google" ]]; then
+
+    gcloud components install gke-gcloud-auth-plugin
+
+    PROJECT_ID=dot-$(date +%Y%m%d%H%M%S)
+
+    echo "export PROJECT_ID=$PROJECT_ID" >> .env
+
+    gcloud projects create ${PROJECT_ID}
+
+    echo "
+Please open https://console.cloud.google.com/marketplace/product/google/container.googleapis.com?project=$PROJECT_ID in a browser and *ENABLE* the API."
+
+    gum input --placeholder "
+Press the enter key to continue."
+
+    echo "
+Please open https://console.cloud.google.com/apis/library/sqladmin.googleapis.com?project=${PROJECT_ID} in a browser and *ENABLE* the API."
+
+    gum input --placeholder "
+Press the enter key to continue."
+
+    export SA_NAME=devops-toolkit
+
+    export SA="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+
+    gcloud iam service-accounts create $SA_NAME \
+        --project $PROJECT_ID
+
+    export ROLE=roles/admin
+
+    gcloud projects add-iam-policy-binding \
+        --role $ROLE $PROJECT_ID --member serviceAccount:$SA
+
+    gcloud iam service-accounts keys create gcp-creds.json \
+        --project $PROJECT_ID --iam-account $SA
+
+    kubectl --namespace crossplane-system \
+        create secret generic gcp-creds \
+        --from-file creds=./gcp-creds.json
+
+    echo "
+apiVersion: gcp.upbound.io/v1beta1
+kind: ProviderConfig
+metadata:
+  name: default
+spec:
+  projectID: $PROJECT_ID
+  credentials:
+    source: Secret
+    secretRef:
+      namespace: crossplane-system
+      name: gcp-creds
+      key: creds" | kubectl apply --filename -
+
+fi
+
+kubectl create namespace a-team
+
+###########
+# Argo CD #
+###########
+
+REPO_URL=$(git config --get remote.origin.url)
+
+yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" argocd/apps.yaml
+
+helm upgrade --install argocd argo-cd \
+    --repo https://argoproj.github.io/argo-helm \
+    --namespace argocd --create-namespace \
+    --values argocd/helm-values.yaml --wait
+
+kubectl apply --filename argocd/project.yaml
+
+kubectl apply --filename argocd/apps.yaml
