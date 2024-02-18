1c835abfdb4e27683822594ee1c64ad13401d203
Viktor Farcic <viktor@farcic.com>
2024-02-18 12:36:06 +0100

    Nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,4 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-
-pkgs.mkShell {
+{ pkgs ? import <nixpkgs> {} }:pkgs.mkShell {
   packages = with pkgs; [
     gum
     gh
