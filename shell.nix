4ca299594aacd0afd7da8d8c894897e7278a68b8
Viktor Farcic <viktor@farcic.com>
2024-03-25 13:06:32 +0100

    Nix Shell

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@
     yq-go
     jq
     (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-    awscli2
+    # awscli2
     azure-cli
     upbound
     teller
@@ -15,4 +15,12 @@
     teller
     kubernetes-helm
   ];
+  shellHook =
+    ''
+      gum style \
+	      --foreground 212 --border-foreground 212 --border double \
+	      --margin "1 2" --padding "2 4" \
+	      'AWS CLI was NOT installed due to a bug in the `awscli2` package.
+Please install it manually.'
+    '';
 }
