1f71af55a785314b83413e743b14a82b047ce2aa
Roy Veshovda <roy@veshovda.no>
2024-03-25 13:24:00 +0100

    fix: patch url if using git/ssh protocol
    Signed-off-by: Roy Veshovda <roy@veshovda.no>

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -242,6 +242,8 @@ kubectl create namespace a-team
 ###########
 
 REPO_URL=$(git config --get remote.origin.url)
+# workaround to avoid setting up SSH key in ArgoCD
+REPO_URL=$(echo $REPO_URL | sed 's/git@github.com:/https:\/\/github.com\//') # replace git@github.com: to https://github.com/
 
 yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" argocd/apps.yaml
 
